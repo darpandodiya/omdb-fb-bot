0a5a6934233f6de82a46e91df4cb8d505fcd253e
Darpan Dodiya <darpan.dodiya@pervacio.com>
2017-07-30 13:10:05 +0530

    Added OMDb API keys.

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
+/*** Index Page ***/
+
 //Index page
 app.get("/", function(req, res) {
     res.send("OMDb chatbot is up and running.");
 });
+
+
+/*** Facebook Messenger Handling ***/
 
 //Facebook webhook used for verification
 app.get("/webhook", function (req, res) {
@@ -35,6 +40,9 @@ app.post("/webhook", function (req, res) {
       entry.messaging.forEach(function(event) {
         if (event.postback) {
           processPostback(event);
+        }
+        else if(event.message) {
+            processMessage(event);
         }
       });
     });
@@ -69,6 +77,44 @@ function processPostback(event) {
       var message = greeting + "My name is OMDb Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
       sendMessage(senderId, {text: message});
     });
+  } else if (payload === "Correct") {
+    sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+  } else if (payload === "Incorrect") {
+    sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
+  }
+}
+
+function processMessage(event) {
+  if (!event.message.is_echo) {
+    var message = event.message;
+    var senderId = event.sender.id;
+
+    console.log("Received message from senderId: " + senderId);
+    console.log("Message is: " + JSON.stringify(message));
+
+    // You may get a text or attachment but not both
+    if (message.text) {
+      var formattedMsg = message.text.toLowerCase().trim();
+
+      // If we receive a text message, check to see if it matches any special
+      // keywords and send back the corresponding movie detail.
+      // Otherwise, search for new movie.
+      switch (formattedMsg) {
+        case "plot":
+        case "date":
+        case "runtime":
+        case "director":
+        case "cast":
+        case "rating":
+          getMovieDetail(senderId, formattedMsg);
+          break;
+
+        default:
+          findMovie(senderId, formattedMsg);
+      }
+    } else if (message.attachments) {
+      sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+    }
   }
 }
 
@@ -85,6 +131,80 @@ function sendMessage(recipientId, message) {
   }, function(error, response, body) {
     if (error) {
       console.log("Error sending message: " + response.error);
+    }
+  });
+}
+
+
+/*** MongoDB & OMDb Handling ***/
+var mongoose = require("mongoose");
+
+var db = mongoose.connect(process.env.MONGODB_URI);
+var Movie = require("./models/movie");
+
+function getMovieDetail(userId, field) {
+  Movie.findOne({user_id: userId}, function(err, movie) {
+    if(err) {
+      sendMessage(userId, {text: "Something went wrong. Try again"});
+    } else {
+      sendMessage(userId, {text: movie[field]});
+    }
+  });
+}
+
+function findMovie(userId, movieTitle) {
+  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle + "&apikey=7e0bbc93", function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var movieObj = JSON.parse(body);
+      if (movieObj.Response === "True") {
+        var query = {user_id: userId};
+        var update = {
+          user_id: userId,
+          title: movieObj.Title,
+          plot: movieObj.Plot,
+          date: movieObj.Released,
+          runtime: movieObj.Runtime,
+          director: movieObj.Director,
+          cast: movieObj.Actors,
+          rating: movieObj.imdbRating,
+          poster_url:movieObj.Poster
+        };
+        var options = {upsert: true};
+        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+          if (err) {
+            console.log("Database error: " + err);
+          } else {
+            message = {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: movieObj.Title,
+                    subtitle: "Is this the movie you are looking for?",
+                    image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                    buttons: [{
+                      type: "postback",
+                      title: "Yes",
+                      payload: "Correct"
+                    }, {
+                      type: "postback",
+                      title: "No",
+                      payload: "Incorrect"
+                    }]
+                  }]
+                }
+              }
+            };
+            sendMessage(userId, message);
+          }
+        });
+      } else {
+          console.log(movieObj.Error);
+          sendMessage(userId, {text: movieObj.Error});
+      }
+    } else {
+      sendMessage(userId, {text: "Something went wrong with OMDb API. Please try again."});
     }
   });
 }
