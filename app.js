72f06c0fac48ea00854683a126c88f88d0e6e6c0
Darpan Dodiya <darpan.dodiya@pervacio.com>
2017-07-30 15:16:35 +0530

    forEach error

--- app.js
+++ app.js
@@ -37,14 +37,17 @@ app.post("/webhook", function (req, res) {
         // There may be multiple entries if batched
         req.body.entry.forEach(function (entry) {
             // Iterate over each messaging event
-            entry.messaging.forEach(function (event) {
-                if (event.postback) {
-                    processPostback(event);
-                }
-                else if (event.message) {
-                    processMessage(event);
-                }
-            });
+
+            if(entry.messaging != undefined) {
+                entry.messaging.forEach(function (event) {
+                    if (event.postback) {
+                        processPostback(event);
+                    }
+                    else if (event.message) {
+                        processMessage(event);
+                    }
+                });
+            }
         });
 
         res.sendStatus(200);
